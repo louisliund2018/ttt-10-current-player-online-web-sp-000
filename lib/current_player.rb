431b91a457428f7cb36617b20f3d396005ac5fd4
Louis Liu <louis.liu2013@gmail.com>
2020-04-09 21:52:56 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,23 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  input.to_i - 1
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def move(board, index, value = "X" or value = "O")
+  board[index] = value
+end
+
 def turn_count
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  
+  
   counter = 0
   if board[index] == "X" or board[index] == "O"
     counter += 1
