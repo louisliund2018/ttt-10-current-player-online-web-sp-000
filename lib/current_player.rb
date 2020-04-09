ae512943349c0c3fbbdc9fd33a6ea0a7fb03578f
Louis Liu <louis.liu2013@gmail.com>
2020-04-09 22:00:03 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,3 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(input)
-  input.to_i - 1
-end
-
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def move(board, index, current_player)
-  board[index] = current_player
-end
-
 def turn_count(board)
   index = input_to_index
   counter = 0
