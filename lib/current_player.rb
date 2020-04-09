a20425bb009c67144a0c8639afb521d4f38806ab
Louis Liu <louis.liu2013@gmail.com>
2020-04-09 22:49:53 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,5 +1,5 @@
 def turn_count(board)
-  board1 = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   counter = 0
   if board[index] == "X" or board[index] == "O"
     counter += 1
