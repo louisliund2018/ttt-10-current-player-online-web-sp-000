c1ac0e4cbd0ed089b6280b1c80f1ed9fd0c98918
Louis Liu <louis.liu2013@gmail.com>
2020-04-09 22:51:48 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,12 @@
 def turn_count(board)
   board = ["X", " ", "O", " ", " ", " ", " ", " ", " "]
-  counter = 0
-  if board[index] == "X" or board[index] == "O"
-    counter += 1
-  else
-    nil
+  board.each do |board|
+    counter = 0
+    if board[index] == "X" or board[index] == "O"
+      counter += 1
+    else
+      nil
+    end
   end
 end
 
